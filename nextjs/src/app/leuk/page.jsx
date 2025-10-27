ff7063811c0fd5b35cd78041bc473aeb5f451103
RickFDND <rick.pijnacker@hva.nl>
2025-10-27 14:54:58 +0100

    nog meer installaties

---
+++ nextjs/src/app/leuk/page.jsx
@@ -0,0 +1,18 @@
+export default function Page() {
+  return (
+    <body>
+        <header>
+            <h1>hello world</h1>
+        </header>
+
+        <main>
+            <section>
+                
+            </section>
+        </main>
+        
+    </body>
+
+        
+  );
+}
